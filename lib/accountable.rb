203a86b5fe9fe3ab221707f278b5be4977c612f8
Alex Thomas <alex.c.thomas@gmail.com>
2012-09-17 14:08:17 +0100

    added module var of parent_controller to inherit from and removed routes

--- lib/accountable.rb
+++ lib/accountable.rb
@@ -11,6 +11,12 @@ module Accountable
   
   mattr_accessor :app_root
   
+  # The parent controller all Accountable controllers inherit from.
+  # Defaults to ApplicationController. This should be set early
+  # in the initialization process and should be set to a string.
+  mattr_accessor :parent_controller
+  @@parent_controller = "ApplicationController"
+  
   def self.setup
     yield self
   end
