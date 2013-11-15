149e199f1636f123cbf2bcb10ba46eded513d821
Alex Thomas <alex.c.thomas@gmail.com>
2013-11-15 16:17:57 +1100

    updated routes and migrations

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_scope :User do
+  get "sign_in", :to => "devise/sessions#new", :as => 'new_user_session'
+  post "sign_in", :to => "devise/sessions#create", :as => 'user_session'
+  delete "sign_out", :to => "devise/sessions#destroy", :as => 'destroy_user_session'
+end
+
+          scope module: 'accountable' do
+          resources :accounts
+          resources :plans, :only => :index
+        end
+
 end
